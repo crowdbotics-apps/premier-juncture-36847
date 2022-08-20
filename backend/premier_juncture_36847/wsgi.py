c4e68d506b22bf944c789d9c20e045768ca60da6
Crowdbotics <team@crowdbotics.com>
2022-08-20 02:35:19 +0000

    Update models

--- backend/premier_juncture_36847/wsgi.py
+++ backend/premier_juncture_36847/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'premier_juncture_36847.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "premier_juncture_36847.settings")
 
 application = get_wsgi_application()
